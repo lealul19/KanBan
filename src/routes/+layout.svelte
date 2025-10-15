8f4a15ecba3d5cb8417198da3fa3edd2a85f83f5
lealul19 <94169536+lealul19@users.noreply.github.com>
2025-10-15 23:19:59 +0200

    add service worker and registration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,23 @@
 <script>
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from "svelte";
 
 	let { children } = $props();
+
+	onMount(() => {
+		if ("serviceWorker" in navigator) {
+			navigator.serviceWorker
+				.register("/service-worker.js")
+				.then(() => console.log(" Service Worker registered"))
+				.catch((err) => console.error("Service Worker failed:", err));
+		}
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="manifest" href="/manifest.json" />
 </svelte:head>
 
 {@render children?.()}
-
